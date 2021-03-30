e73629e94a3d21d1d858c5613db6283995a77404
Daniel Castilho <dan.castilho@gmail.com>
2021-03-30 01:10:22 -0400

    Initial Code Splitting

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  }
+
+  render() {
+    switch (this.state.route) {
+      case 'page1':
+        return <Page1 onRouteChange={ this.onRouteChange } />
+      case 'page2':
+        return <Page2 onRouteChange={ this.onRouteChange } />
+      case 'page3':
+        return <Page3 onRouteChange={ this.onRouteChange } />
+      default:
+        return
+    }
+  }
+}
+
+export default App;
